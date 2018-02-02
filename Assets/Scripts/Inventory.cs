9db4b30e5778ad23548fc6a70cd41909137a8cd4
Dmitrij <dmitrij1699@gmail.com>
2018-02-02 04:17:06 +0300

    Первый набросок инвентаря

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+	GameObject inventoryPanel;
+	GameObject slotPanel;
+	ItemDatabase database;
+	public GameObject inventorySlot;
+	public GameObject inventoryItem;
+
+	int slotAmount;
+	public List<Item> items = new List<Item> ();
+	public List<GameObject> slots = new List<GameObject> ();
+
+	void Start(){
+		database = GetComponent<ItemDatabase> ();
+
+		slotAmount = 20;
+		inventoryPanel = GameObject.Find ("Inventory Panel");
+		slotPanel = inventoryPanel.transform.Find ("Slot Panel").gameObject;
+		for (int i = 0; i < slotAmount; i++) {
+			items.Add (new Item ());
+			slots.Add (Instantiate (inventorySlot));
+			slots [i].transform.SetParent (slotPanel.transform);
+		}
+
+		AddItem (0);
+		AddItem (1);
+	}
+
+	public void AddItem(int id){
+		Item itemToAdd = database.FetchItemByID (id);
+		for (int i = 0; i < items.Count; i++) {
+			if (items [i].ID == -1) {
+				items [i] = itemToAdd;
+				GameObject itemObj = Instantiate (inventoryItem);
+				itemObj.transform.SetParent (slots [i].transform);
+				itemObj.GetComponent<Image> ().sprite = itemToAdd.Sprite;
+				itemObj.transform.position = Vector2.zero;
+				itemObj.name = itemToAdd.title;
+				break;
+			}
+		}
+	}
+}
