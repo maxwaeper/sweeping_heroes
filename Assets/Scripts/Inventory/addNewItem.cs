58020480b833f9e4684d1e91ddb7c53b8d7d63d9
Dmitrij <dmitrij1699@gmail.com>
2018-02-17 17:52:46 +0300

    подобрать шмоточку
    со вкусом багов

---
+++ Assets/Scripts/Inventory/addNewItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addNewItem : MonoBehaviour {
+	public int ID;
+	GameObject inventory;
+
+	// Use this for initialization
+	void Start () {
+		inventory = GameObject.Find ("Inventory");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			inventory.GetComponent<Inventory> ().AddItem (ID);
+			Destroy (GameObject.Find( ID.ToString() + "_item"  ));
+		}
+	}
+}
